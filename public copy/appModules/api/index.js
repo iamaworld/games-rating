3a5bae1bc1f4bacb299c50d3ec2a7b834becb0d4
iamheaven <125787194+iamaworld@users.noreply.github.com>
2024-04-30 15:01:06 +0500

    Delete public copy directory

--- public copy/appModules/api/index.js
+++
@@ -1,9 +0,0 @@
-const { getData } = require("./ali-utils");
-const endpoints = require("./config");
-const { getRandomGame } = require("./ali-utils");
-
-module.exports = {
-  getData,
-  endpoints,
-  getRandomGame,
-};
--- public copy/appModules/rating/index.js
+++
@@ -1,9 +0,0 @@
-const config = require("./config");
-const makeRatingFile = require("./rating-file");
-const updateRating = require("./calculations");
-
-module.exports = {
-  makeRatingFile,
-  config,
-  updateRating,
-};
